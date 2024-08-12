7568ceebc71ac5b349410e69c3e6f299244511d5
yukui5401 <brookeyang5401@gmail.com>
2024-08-11 21:38:18 -0400

    fixed email issue

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -29,7 +29,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "b239yang@uwaterloo.ca",
+      to: "brookeyang5401@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
