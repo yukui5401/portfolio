15f986959ef65e734483aa1cc5131138a8fb2534
yukui5401 <brookeyang5401@gmail.com>
2024-08-24 21:21:03 -0400

    removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,12 @@
 import Header from "@/components/header";
-import {
-  Nunito,
-  Open_Sans,
-  Lato,
-  Montserrat,
-  Oswald,
-  Poppins,
-  Cormorant_Garamond,
-} from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
-const nunito = Nunito({ subsets: ["latin"] });
 const openSans = Open_Sans({ subsets: ["latin"] });
-const lato = Lato({
-  subsets: ["latin"],
-  weight: "300",
-});
-const montserrat = Montserrat({ subsets: ["latin"] });
-const oswald = Oswald({ subsets: ["latin"] });
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: "300",
-});
-const cormorant_garamond = Cormorant_Garamond({
-  subsets: ["latin"],
-  weight: "300"
-});
 
 export const metadata = {
   title: "Brooke Yang | Portfolio",
